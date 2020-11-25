279ec50fdf00a573adc627b2f1e0d1ed6ff3107f
phoudail <philemon.houdaille@free.fr>
2020-11-25 12:00:32 +0100

    Added the couples utility class

---
+++ src/mj/Couples.java
@@ -0,0 +1,13 @@
+package mj;
+
+public class Couples<T1, T2> {
+
+    public T1 first;
+    public T2 second;
+
+    public Couples(T1 first, T2 second) {
+        this.first = first;
+        this.second = second;
+    }
+    
+}
