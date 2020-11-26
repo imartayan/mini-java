e4d4a7295bd68b5535f5b9dd0e7c8880804a48bd
Igor Martayan <igor@martayan.org>
2020-11-26 15:45:27 +0100

    Update Method TypeCheck Test

--- tests/ok/TypecheckMethod.java
+++ tests/ok/TypecheckMethod.java
@@ -4,6 +4,7 @@ class CheckMethod{
     }
 }
 
+// Check argument type verification
 class CheckMethodA{
     public int sum(int a, boolean b, int c) {
         return a + c;
@@ -12,5 +13,39 @@ class CheckMethodA{
         int res;
         res = this.sum(1, true, 2);
         return res;
+    }
+}
+
+class CheckMethodObject {
+    int value;
+    public int setValue(int val) {
+        value = val;
+        return value;
+    }
+}
+
+class CheckMethodObjectPriority extends CheckMethodObject {
+    int priority;
+    public int setPriority(int p) {
+        priority = p;
+        return priority;
+    }
+}
+
+// Check that argument is a subtype of the expected type
+class CheckMethodB {
+    public int sumValues(CheckMethodObject a, CheckMethodObject b) {
+        int valA;
+        int valB;
+        valA = a.setValue(1);
+        valB = b.setValue(2);
+        return valA + valB;
+    }
+    public int test() {
+        CheckMethodObjectPriority objA;
+        CheckMethodObject objB;
+        objA = new CheckMethodObjectPriority();
+        objB = new CheckMethodObject();
+        return this.sumValues(objA, objB);
     }
 }
