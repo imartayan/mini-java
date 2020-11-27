a5bc018a088ad711b31a978b785e5aa82df05200
phoudail <philemon.houdaille@free.fr>
2020-11-27 16:54:59 +0100

    Improved test results readability

--- src/mj/test/Test.java
+++ src/mj/test/Test.java
@@ -14,32 +14,20 @@ import mj.Interpreter;
 import mj.ExecError;
 
 public class Test {
-    private static String[] validFiles = {
-        "tests/ok/TestProgram.java",
-        "tests/ok/TestMain.java",
-        "tests/ok/TestClass.java",
-        "tests/ok/TestMethod.java",
-        "tests/ok/TestAllocation.java",
-        "tests/ok/OperationTest.java",
-        "tests/ok/BinaryTree.java",
-        "tests/ok/BubbleSort.java",
-        "tests/ok/Factorial.java",
-        "tests/ok/LinearSearch.java",
-        "tests/ok/MoreThan4.java",
-        "tests/ok/QuickSort.java",
-        "tests/ok/TreeVisitor.java"
-    };
 
-    private static String[] invalidFiles = {
-        "tests/error/TypecheckNotInitialized.java",
-        "tests/error/BinaryTree-error.java",
-        "tests/error/BubbleSort-error.java",
-        "tests/error/Factorial-error.java",
-        "tests/error/LinearSearch-error.java",
-        "tests/error/MoreThan4-error.java",
-        "tests/error/QuickSort-error.java",
-        "tests/error/TreeVisitor-error.java"
-    };
+    private static int passedTests;
+
+    private static String[] validFiles = { "tests/ok/TestProgram.java", "tests/ok/TestMain.java",
+            "tests/ok/TestClass.java", "tests/ok/TestMethod.java", "tests/ok/TestAllocation.java",
+            "tests/ok/OperationTest.java", "tests/ok/BinaryTree.java", "tests/ok/BubbleSort.java",
+            "tests/ok/Factorial.java", "tests/ok/LinearSearch.java", "tests/ok/MoreThan4.java",
+            "tests/ok/QuickSort.java", "tests/ok/TreeVisitor.java" };
+
+    private static String[] invalidFiles = { "tests/error/TypecheckNotInitialized.java",
+            "tests/error/BinaryTree-error.java", "tests/error/BubbleSort-error.java",
+            "tests/error/Factorial-error.java", "tests/error/LinearSearch-error.java",
+            "tests/error/MoreThan4-error.java", "tests/error/QuickSort-error.java",
+            "tests/error/TreeVisitor-error.java" };
 
     private static void runPositiveTests() {
         for (String filename : validFiles) {
@@ -48,10 +36,13 @@ public class Test {
                 TypeChecker t = new TypeChecker();
                 System.out.println("Typechecking " + filename);
                 p.typeCheck(t);
+                System.out.println("No type errors found");
                 Interpreter interp = new Interpreter(p);
                 SimpleHeap h = new SimpleHeap(interp);
                 System.out.println("Evaluating " + filename);
                 interp.run(h);
+                System.out.println("No runtime errors found");
+                passedTests++;
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             } catch (TypeError e) {
@@ -62,6 +53,7 @@ public class Test {
                 e.printStackTrace();
             }
         }
+
     }
 
     private static void runNegativeTests() {
@@ -71,22 +63,28 @@ public class Test {
                 TypeChecker t = new TypeChecker();
                 System.out.println("Typechecking " + filename);
                 p.typeCheck(t);
+                System.out.println("No type error found, executing program instead");
                 Interpreter interp = new Interpreter(p);
                 SimpleHeap h = new SimpleHeap(interp);
                 System.out.println("Executing " + filename);
                 interp.run(h);
+                System.out.println("No errors found even though the program should fail.");
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             } catch (TypeError e) {
-                System.out.println("TypeError as expected");
+                System.out.println("TypeError as expected for " + filename);
+                passedTests++;
             } catch (ExecError e) {
-                System.out.println("ExecError as expected");
+                System.out.println("ExecError as expected for " + filename);
+                passedTests++;
             }
         }
     }
 
     public static void main(String[] arg) {
+        passedTests = 0;
         runPositiveTests();
         runNegativeTests();
+        System.out.println(passedTests + " tests passed out of " + (validFiles.length + invalidFiles.length));
     }
 }
